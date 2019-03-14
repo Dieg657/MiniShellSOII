16fbd3a5ce0cf5d5e52c492f8eec9cf756163238
Diego Soares <dieg657@gmail.com>
2019-03-14 01:38:00 -0300

    Adicionado arquivos

---
+++ minishell.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    // char * linha= malloc(sizeof(char)*50);
+    // char * comando= malloc(sizeof(char)*50);
+    // for(;;)
+    // {
+    //         printf("loldigow# ");
+    //         scanf("%s",linha);
+    //         execlp(linha, linha,"aux",0);
+    // }
+    // // scanf("%s",&linha);
+    // printf("%s",&comando);
+    // string LinhadeComando="";
+    // string comando="";
+    // string argumentos="";
+    // for(;;)
+    // {
+    //     cout<<"loldigow# ";
+    //     cin>>LinhadeComando;
+    //     cout<<LinhadeComando;
+    // }
+
+
+
+}
+
+
+
+
+
+
+
+
+// #include <unistd.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// int main()
+// {
+//     printf("Running ps with execlp\n");
+//     execlp("ps", "ps", "ax", 0);
+//     printf("Done.\n");
+//     exit(0);
+// }
