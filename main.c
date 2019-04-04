4e9207b7386104c859b2a0873ce2ae4bdaafe021
Diego Soares <dieg657@gmail.com>
2019-04-04 18:15:24 -0300

    Adicionando redirecionamento de arquivo

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
-#include<sys/wait.h>
-#include <stdio.h>
+#include <sys/wait.h>
 #include <signal.h>
+
 
 /*
  * Alunos: Diego dos Santos Soares
@@ -13,9 +14,38 @@
  *
  *
  */
+
 #define max 100
 int exec=0;
 int back=0;
+struct sigaction act;
+int entrada_de_arquivo=0;
+int saida_de_arquivo=0;
+char nome_arquivo[max];
+int pos_nome_arquivo=0;
+
+int aux(char *** argumentos){
+   int fda[2];
+   pipe(fda);
+   switch(fork()) {
+
+           case 0:
+               close(1);
+               dup(fda[1]);
+               close(fda[1]);
+               close(fda[0]);
+               execvp(argumentos[0][0],argumentos[0]);
+               return 1;
+
+           default:
+               close(0);
+               dup(fda[0]);
+               close(fda[0]);
+               close(fda[1]);
+               execvp(argumentos[1][0],argumentos[0]);
+               return 0;
+   }
+}
 
 void clean(char * params, int fim);
 
@@ -27,118 +57,190 @@ int main(){
     pid_t pid; //variavel para guardar id de processo
     signal(SIGINT,&trataSignal);
 
-
     for(;;) //Loop de Iteração do Shell
     {
-            char * linha= malloc(sizeof(char)*100);    //variavel para receber a linha de comando
-            char ** argumentos=malloc(sizeof(char*)*10); //matriz para receber argumentos de chamada de sistema
-            for(int i=0; i<10; i++)argumentos[i]=malloc(sizeof(char)*15); // faz parte declaração da matriz
-                                                        //onde vai guardar os argumentos
-            clean(linha,100);
-            printf("MINISHELL> ");
-            //Efetua a leitura de dados do usuário!
-            scanf(" %[^\n]s", linha);
+           char * linha= malloc(sizeof(char)*100);
+           char ** argumentos=malloc(sizeof(char*)*10);
+           char *** lista_de_argumentos=malloc(sizeof(char**)*2);
+           int lista_de_argumentos_atual=0;
 
+           for(int j=0; j<5;j++)
+           {
+               lista_de_argumentos[j]=malloc(sizeof(char*)*10);
+               for(int i=0; i<10; i++)
+               {
+                   argumentos[i]=malloc(sizeof(char)*15);
+                   lista_de_argumentos[j][i]=malloc(sizeof(char)*15);
+               }
+           }
 
-            setbuf(stdin, NULL);
+           clean(linha,100);
+           printf("[MINISHELL]$: ");
+           //Efetua a leitura de dados do usuário!
+           scanf(" %[^\n]s", linha);
 
-            if(isolaArgumentos(argumentos,linha)==1)
-            {
-                      if(strcmp(argumentos[0],"sair")==0 || strcmp(argumentos[0],"exit")==0
-                      || strcmp(argumentos[0], "EXIT")==0)
-                      {
-                          break;
-                      }
-                      else if(strcmp(argumentos[0],"cd")==0)
-                      {
+           setbuf(stdin, NULL);
 
-                             chdir(argumentos[1]);
-                      }
-                      else if(exec==1){
-                          /*
-                           * Se a flag de exec estivar ativa, ele pula para salto, uma label declarada um pouco antes
-                           * da chamada do execvp para ser executada e depois morta
-                           */
-                          goto SALTO;
-                      }
-                    else{
-                        pid=fork();     //id do processo filho
-                        if(pid==0) // Executa o comando
-                        {
-                            SALTO:
-                            if(execvp(argumentos[0],argumentos)==-1){
-                                printf("Comando invalido\n");
-                                exit(0);
-                            }
-                        }else if(back==1){ // Entra para aguardar o filho!
-                            if(waitpid(pid, 0, 0) < 0){
-                                printf("\nFilho nao respondeu!");
-                            }else{
-                                back = 0; //Reseta a flag
-                                printf("\nFilho finalizou!");
-                            }
-                        }
+           int debug=isolaArgumentos(lista_de_argumentos[lista_de_argumentos_atual],linha);
+           while(debug==2)
+           {
+               lista_de_argumentos_atual++;
+               debug=isolaArgumentos(lista_de_argumentos[lista_de_argumentos_atual],linha);
+           }
+           if(debug==1)
+           {
+                     if(strcmp(argumentos[0],"sair")==0 || strcmp(argumentos[0],"exit")==0)
+                     {
+                         break;
+                     }
+                     else if(strcmp(argumentos[0],"cd")==0)
+                     {
+                            chdir(argumentos[1]);
+                     }
+                     else if(exec==1)
+                     {
+                           goto SALTO;
+                     }
+                   else{
+                       pid=fork();
+                       if(pid==0)
+                       {
+                           SALTO:
+                           if(saida_de_arquivo==1)
+                           {
+                               int fd;
+                               fd = open(nome_arquivo,O_RDWR|O_CREAT|O_TRUNC,0666);
+                               close(1);
+                               dup(fd);
+                           }
+                           else if(entrada_de_arquivo==1)
+                           {
+                               int fd;
+                               fd = open(nome_arquivo,0);
+                               close(0);
+                               dup(fd);
+                           }
+
+                           if(lista_de_argumentos_atual==0)
+                           {
+                               if(execvp(lista_de_argumentos[0][0],lista_de_argumentos[0])==-1){
+                                   printf("Comando innvalido!\n");
+                                   exit(0);
+                               }
+                           }
+                           else{
+                               int retorno=aux(lista_de_argumentos);
+                               if(retorno==1)return 0;
+
+                           }
+                        }else if(back==0){
+                           saida_de_arquivo=0;
+                           clean(nome_arquivo,max);
+                           pos_nome_arquivo=0;
+                           entrada_de_arquivo=0;
+                           waitpid(pid,0,0);
+                       }
                     }
-            }
-            else printf("Parametros Invalidos");
-
+           }
+           else printf("quantidade de parametros invalidos");
     }
     printf("Estou finalizando");
 }
 
+
 void trataSignal(int sig) {
-    char c;
-    signal(sig, SIG_IGN);
-    printf("\nSinal capturado: %d", sig);
-    printf("\nCtrl-C? [s/n]");
-    c = getchar();
-    if(c=='s' || c=='S')
-        exit(0);
-    else{
-        main();
-    }
+    for(;;)
+      {
+          printf("Você deseja realmente sair: s-sim n-nao\n");
+          setbuf(stdin, NULL);
+          char aux=getchar();
+          if(aux=='S' || aux=='s')
+          {
+              exit(0);
+          }
+          if(aux=='N' || aux=='n')
+          {
+              break;
+          }
+      }
+      setbuf(stdin, NULL);
+      printf("# ");
 }
 
 int isolaArgumentos(char ** params, char * cadeia)    //Função para separar varios argumentos
 {                                           //na mesma linha onde encontrar espaços
     int v=0;
-    for(int i=0; i<9;i++)clean(params[i],25);   //Chama funcao para limpar lixos na matriz de argumentos
-    int i,j;
-
-    for(i=0, j=0 ; i<max; i++, j++)         //Laço de iteração até o final da linha de comando
-    {
-        if(cadeia[i]==' ')                  //se durante a varredura da linha do comando
-         {                                  //ele encontrar ' ', executa...
-             j=-1;                          //
-             v++;                           //
-             if(v>=10)                      //suporta somente 10 argumentos
-             {
-                 return 0;                  //se passar, para de ler codigos
-             }
-             if(strcmp(params[0],"exec")==0) //se o primeiro argumento for == "exec"
-             {                               //ativa uma flag global que usamos para
-                clean(params[0],10);        //chamar o codigo e depois morrer
-                exec=1;
-                v--;
-             }
-
-         }
-         else{
-             params[v][j]=cadeia[i];      //a medida que interamos em i e j , o argumento na posicao v
-         }                                //vai receber este texto
-    }
-    if(strcmp(params[v],"&")==0)          //se no ultimo argumento
-    {                                     //encontrar  '&' ativa uma flag
-        back=1;                           //para desativar o wait do processo filho
-        v--;
-    }
-    v++;
-    while(v<10)
-    {
-        params[v]=0;                     //adiciona 0 na posicao do argumento na matriz
-        v++;
-    }
-    return 1; //Isolou os parametros corretamente
+       for(int i=0; i<9;i++)clean(params[i],25);   //chama funcao para limpar lixos na matriz de
+       int i,j;                                    //argumentos
+       for(i=0, j=0 ; i<max; i++, j++) {         ///interage ate o final da linha de comando
+             if(cadeia[i]==0)
+           {
+               j--;
+           }
+           else if(cadeia[i]==' '){                  ///se durante a varredura da linha do comando
+                                             ///ele encontrar ' ', executa...
+                j=-1;                          //
+                v++;                           //
+                if(v>=10)                      //suporta somente 10 argumentos
+                {
+                    return 0;                  //se passar, para de ler codigos
+                }
+                if(strcmp(params[0],"exec")==0)//se o primeiro argumento for == "exec"
+                {                              //ativa uma flag global que usamos para
+                    clean(params[0],10);       //chamar o codigo e depois morrer
+                   exec=1;
+                   v--;
+                }
+            }
+            else{
+               if(cadeia[i]=='<')
+               {
+                   entrada_de_arquivo=1;
+                   i++;
+                   v--;
+                 }
+               else if(cadeia[i]=='>')
+               {
+                   saida_de_arquivo=1;
+                   i++;
+                   v--;
+               }
+               else if(cadeia[i]=='|')
+               {
+                   cadeia[i]=0;
+                   cadeia[i+1]=0;
+                       while(v<10)
+                       {
+                           params[v]=0;
+                           v++;
+                       }
+                   return 2;
+               }
+               else if(cadeia[i]=='\0')break;
+               else if(entrada_de_arquivo==1 || saida_de_arquivo==1)
+               {
+                   nome_arquivo[pos_nome_arquivo]=cadeia[i];
+                   pos_nome_arquivo++;
+               }
+               else
+               {
+                    params[v][j]=cadeia[i];
+               }
+            }
+            cadeia[i]=0;
+       }
+       if(strcmp(params[v],"&")==0)          //se no ultimo argumento
+       {                                     //encontrar  '&' ativa uma flag
+           back=1;                           //para desativar o wait do proscesso filho
+           v--;
+       }
+       v++;
+       while(v<10)
+       {
+           params[v]=0;                     //adiciona 0 na posicao do argumento na matriz
+           v++;
+       }
+       return 1; //Isolou os parametros corretamente
 }
 
 void clean(char * params, int fim)  //função para limpar string de argumentos
